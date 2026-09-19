fb5dfc4b7af262e893a777cf192364b2b075b4ca
Dahyun Park <howielyn23@gmail.com>
2026-09-20 02:37:58 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
-        jcenter()
         mavenCentral()
-        maven(url = "https://artifactory.img.ly/artifactory/imgly")
     }
 }
 dependencyResolutionManagement {
@@ -14,17 +12,9 @@ dependencyResolutionManagement {
         jcenter()
         mavenCentral()
         maven { setUrl("https://jitpack.io") }
-        maven(url = "https://artifactory.img.ly/artifactory/imgly")
     }
 }
 
 rootProject.name = "Simple-Gallery"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
-
-// TODO: This will be deprecated in future. Migrate to the newer `pluginManagement { includeBuild() }` mechanism instead of explicitly substituting dependency.
-/*includeBuild("../Simple-Commons") {
-    dependencySubstitution {
-        substitute(module("com.github.SimpleMobileTools:Simple-Commons")).using(project(":commons"))
-    }
-}*/
